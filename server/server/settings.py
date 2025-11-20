82d86b25b55d0753f00ce89a9d1463a7485d9965
Solomon Tchaya <stchaya@baylormw.org>
2025-11-20 08:41:54 +0200

    Fix Database

--- server/server/settings.py
+++ server/server/settings.py
@@ -129,6 +129,11 @@ DATABASES = {
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
         'PORT': os.getenv('DB_PORT'),
+
+        'OPTIONS': {
+            'sslmode': 'require',
+        },
+        'CONN_HEALTH_CHECKS': False,
     }
 }
 
