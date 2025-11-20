e58ec1449e736afc7e12e4269ebeddd972df6e80
Solomon Tchaya <stchaya@baylormw.org>
2025-11-20 08:36:10 +0200

    Fix Database

--- server/server/settings.py
+++ server/server/settings.py
@@ -123,7 +123,7 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
